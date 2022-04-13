b97127546aa400997b8e7f9d3533319dba68cb2b
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-13 13:03:42 +0300

    Task_01. 3 items

--- src/main/java/HomeTasks/task_01/HomeWorkApp.java
+++ src/main/java/HomeTasks/task_01/HomeWorkApp.java
@@ -4,11 +4,24 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
         printThreeWords();
+        System.out.println("-----------------------------------");
+        checkSumSign();
 
     }
 
     private static void printThreeWords() {
-        System.out.println("Orange\n" + "Banana\n" + "Apple\n");
+        System.out.println("Orange\n" + "Banana\n" + "Apple");
     }
+
+    private static void checkSumSign() {
+
+        float a = 20.2f;
+        double b = 19.8;
+        String result = ((a + b) > 0) ? "Summ is positive" : "Summ is negative";
+        System.out.println(result);
+
+    }
+
+
 
 }
