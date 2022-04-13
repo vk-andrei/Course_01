638df8e6fc4ec55c55c36125dad4feec3854ca60
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-13 10:42:15 +0300

    Task_01. 1-2 items

---
+++ src/main/java/HomeTasks/task_01/HomeWorkApp.java
@@ -0,0 +1,14 @@
+package HomeTasks.task_01;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+
+        printThreeWords();
+
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange\n" + "Banana\n" + "Apple\n");
+    }
+
+}
