6644e287785b3b88c9f4c99bfc71331ddba6fa72
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-13 14:36:35 +0300

    Task_01. 4 items. HomeWork is done.

--- src/main/java/HomeTasks/task_01/HomeWorkApp.java
+++ src/main/java/HomeTasks/task_01/HomeWorkApp.java
@@ -2,11 +2,31 @@ package HomeTasks.task_01;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
         printThreeWords();
         System.out.println("-----------------------------------");
         checkSumSign();
+        System.out.println("-----------------------------------");
+        printColor();
+        System.out.println("-----------------------------------");
+        compareNumbers();
+    }
 
+    private static void compareNumbers() {
+        double a = -100 + Math.random() * 201;
+        double b = -100 + Math.random() * 201;
+        System.out.println("a = " + a + "; b = " + b);
+        System.out.println((a >=b ) ? "a >= b" : "a < b");
+    }
+
+    private static void printColor() {
+        int value = - 200 + (int) (Math.random() * 401); // [-200..200]
+        if (value <= 0) {
+            System.out.println("Red! (" + value + ")");
+        } else if (value < 100) {
+            System.out.println("Yellow! (" + value + ")");
+        } else {
+            System.out.println("Green! (" + value + ")");
+        }
     }
 
     private static void printThreeWords() {
@@ -14,14 +34,9 @@ public class HomeWorkApp {
     }
 
     private static void checkSumSign() {
-
         float a = 20.2f;
         double b = 19.8;
         String result = ((a + b) > 0) ? "Summ is positive" : "Summ is negative";
         System.out.println(result);
-
     }
-
-
-
 }
