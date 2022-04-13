d70c24f216c962e85c344777e0b911a2dcedeb6c
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-13 15:06:19 +0300

    Task_01. 5 items. HomeWork is done!

--- src/main/java/HomeTasks/task_01/HomeWorkApp.java
+++ src/main/java/HomeTasks/task_01/HomeWorkApp.java
@@ -9,17 +9,19 @@ public class HomeWorkApp {
         printColor();
         System.out.println("-----------------------------------");
         compareNumbers();
+        System.out.println("-----------------------------------");
+        checkLeapYear(2022);   // Задание из методички. Високосный год
     }
 
     private static void compareNumbers() {
         double a = -100 + Math.random() * 201;
         double b = -100 + Math.random() * 201;
         System.out.println("a = " + a + "; b = " + b);
-        System.out.println((a >=b ) ? "a >= b" : "a < b");
+        System.out.println((a >= b) ? "a >= b" : "a < b");
     }
 
     private static void printColor() {
-        int value = - 200 + (int) (Math.random() * 401); // [-200..200]
+        int value = -200 + (int) (Math.random() * 401); // [-200..200]
         if (value <= 0) {
             System.out.println("Red! (" + value + ")");
         } else if (value < 100) {
@@ -38,5 +40,13 @@ public class HomeWorkApp {
         double b = 19.8;
         String result = ((a + b) > 0) ? "Summ is positive" : "Summ is negative";
         System.out.println(result);
+    }
+
+    public static void checkLeapYear(int year) {
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            System.out.println("This year is LEAP");
+        } else {
+            System.out.println("This year is COMMON");
+        }
     }
 }
