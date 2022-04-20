54de61ac456e8c04d4208e53aa566b05fa6931e3
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-20 15:34:24 +0300

    HomeWork_03. Task_4. Done.

--- src/main/java/HomeTasks/task_03/HomeWork_03.java
+++ src/main/java/HomeTasks/task_03/HomeWork_03.java
@@ -17,9 +17,51 @@ public class HomeWork_03 {
         System.out.println("--------------------");
 
         System.out.println("TASK 2:");
-        int[] myArr2 = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] myArr2 = new int[100];
+        for (int i = 0; i < myArr2.length; i++) {
+            myArr2[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(myArr2));
+        System.out.println("--------------------");
+
+        System.out.println("TASK 3:");
+        int[] myArr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("WAS: " + Arrays.toString(myArr3));
+        for (int i = 0; i < myArr3.length; i++) {
+            if (myArr3[i] < 6) {
+                myArr3[i] *= 2;
+            }
+        }
+        System.out.println("NOW: " + Arrays.toString(myArr3));
+        System.out.println("--------------------");
+
+        System.out.println("TASK 4:");
+        int[][] myArr4 = new int[5][5];
+        for (int[] row : myArr4) {
+            Arrays.fill(row, 9);
+        }
+        System.out.println("WAS:");
+        for (int[] row : myArr4) {
+            System.out.println(Arrays.toString(row));
+        }
+        for (int i = 0; i < myArr4.length; i++) {
+            for (int j = 0; j < myArr4[0].length; j++) {
+                if (i == j) {
+                    myArr4[i][j] = 1;
+                }
+                if (i + j == myArr4.length - 1) {
+                    myArr4[i][j] = 1;
+                }
+            }
+        }
+        System.out.println("NOW:");
+        for (int[] row : myArr4) {
+            System.out.println(Arrays.toString(row));
+        }
 
 
 
-    }
+
+
+        }
 }
