43256da2fad8124e195c43cbe7bf6caf1bd5f044
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-20 15:07:46 +0300

    HomeWork_03. Task_1. Done.

--- src/main/java/HomeTasks/task_03/HomeWork_03.java
+++ src/main/java/HomeTasks/task_03/HomeWork_03.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class HomeWork_03 {
     public static void main(String[] args) {
 
-        // TASK 1
+        System.out.println("TASK 1:");
         int[] myArr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("WAS: " + Arrays.toString(myArr));
         for (int i = 0; i < myArr.length; i++) {
             if (myArr[i] == 1) {
                 myArr[i] = 0;
@@ -12,8 +13,12 @@ public class HomeWork_03 {
                 myArr[i] = 1;
             }
         }
-        System.out.println(Arrays.toString(myArr));
+        System.out.println("NOW: " + Arrays.toString(myArr));
         System.out.println("--------------------");
+
+        System.out.println("TASK 2:");
+        int[] myArr2 = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
 
 
     }
