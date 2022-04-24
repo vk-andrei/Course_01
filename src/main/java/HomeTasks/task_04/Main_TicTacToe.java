d33855ec27afdadf3135c9e4ad37a6b5a8731226
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-04-24 06:46:28 +0300

    HomeWork_04. Branch-2. TicTacToe. Done all besides DIAGs

---
+++ src/main/java/HomeTasks/task_04/Main_TicTacToe.java
@@ -0,0 +1,7 @@
+package HomeTasks.task_04;
+
+public class Main_TicTacToe {
+    public static void main(String[] args) {
+        TicTacToe.game_TicTacToe();
+    }
+}
--- src/main/java/HomeTasks/task_04/TicTacToe.java
+++ src/main/java/HomeTasks/task_04/TicTacToe.java
@@ -2,132 +2,245 @@ package HomeTasks.task_04;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TicTacToe {
 
+    static int size;
+    static char[][] map;
+    static int dots_to_win;
+    static final char DOT_EMPTY = '•';
+    static final char DOT_HUMAN = 'X';
+    static final char DOT_TERMINATOR = 'O';
+    static int turns_counter;
+    static int lastRow;
+    static int lastColomn;
 
-    public static final int SIZE = 3;
-    public static char[][] map;
-    public static final int DOTS_TO_WIN = 3;
-    public static final char DOT_EMPTY = '•';
-    public static final char DOT_X = 'X';
-    public static final char DOT_O = 'O';
-    public static final Scanner SCAN = new Scanner(System.in);
-    public static final Random RAND = new Random();
+    static final String HEADER_FIRST_SYMBOL = " #";
 
-    public static void main(String[] args) {
+    static final Scanner SCAN = new Scanner(System.in);
+    static final Random RAND = new Random();
 
-        initMap();
-        printMap();
+    public static void game_TicTacToe() {
+
+        do {
+            System.out.println("THE GAME STARTS");
+            initGame();
+            printMap();
+            playGame();
+        } while (isContinueGame());
+        endGame();
+    }
+
+
+    private static void playGame() {
         while (true) {
-            humanTurn();
-            printMap();
-            if (checkWin(DOT_X)) {
-                System.out.println("Human WINS!");
+
+            humanTurn();                    // 1.
+            printMap();                     // 2.
+            if (checkEnd(DOT_HUMAN))        // 3.
                 break;
-            }
-            if (isMapFull()) {
-                System.out.println("DRAW!");
+            terminatorTurn();               // 4.
+            printMap();                     // 5.
+            if (checkEnd(DOT_TERMINATOR))   // 6.
                 break;
-            }
-            terminatorTurn();
-            printMap();
-            if (checkWin(DOT_O)) {
-                System.out.println("Terminator WINS!");
-                break;
-            }
-            if (isMapFull()) {
-                System.out.println("DRAW!");
-                break;
-            }
         }
-        System.out.println("GAME OVER!");
+    }
+
+    private static void initGame() {
+        turns_counter = 0;
+        size = inputFromScanner();
+        dots_to_win = chooseDotsToWin(size);
+        System.out.println("Get " + dots_to_win + " chips in a line to win.");
+        map = new char[size][size];
+        initMap();
+
+    }
+
+    private static int inputFromScanner() {
+        while (true) {
+            System.out.print("Enter game size (3 - 20): ");
+            if (SCAN.hasNextInt()) {
+                return SCAN.nextInt();
+            }
+            System.out.println(SCAN.next() + " - not a number!");
+        }
     }
 
     private static void initMap() {
-        map = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 map[i][j] = DOT_EMPTY;
             }
         }
     }
 
+    private static int chooseDotsToWin(int size) {
+        return switch (size) {
+            case 1, 2, 3, 4, 5 -> 3;
+            case 6, 7, 8, 9, 10 -> 4;
+            default -> 5;
+        };
+    }
+
     private static void printMap() {
-        System.out.print("  ");
-        for (int i = 0; i < SIZE; i++) {
-            System.out.print((i + 1) + " ");
+        System.out.print(HEADER_FIRST_SYMBOL);
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%3d", (i + 1));
         }
         System.out.println();
-        for (int i = 0; i < SIZE; i++) {
-            System.out.print((i + 1) + " ");
-            for (int j = 0; j < SIZE; j++) {
-                System.out.print(map[i][j] + " ");
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%2d", (i + 1));
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%3c", map[i][j]);
             }
             System.out.println();
         }
     }
 
     private static void humanTurn() {
-        String str = "";
+        System.out.println("HUMAN TURN");
+        String str;
+        String regex = "\\d{1,2}\\s\\d{1,2}";
+        Pattern myPattern = Pattern.compile(regex);         // Pattern for checking
+        boolean checkPattern;
         int x, y;
         do {
-            System.out.print("Input X & Y (throw space): ");
-            str = SCAN.nextLine();
+            do {
+                System.out.print("Input row & column (throw 1 space): ");
+                str = SCAN.nextLine();
+                Matcher myMatcher = myPattern.matcher(str);
+                checkPattern = myMatcher.matches();
+            } while (!checkPattern);
             String[] strArr = str.split(" ");
             x = Integer.parseInt(strArr[0]) - 1;
             y = Integer.parseInt(strArr[1]) - 1;
         } while (!isValidCell(x, y));
-        map[y][x] = DOT_X;
+        map[x][y] = DOT_HUMAN;
+        lastRow = x;
+        lastColomn = y;
+        turns_counter++;
+    }
+
+    private static boolean checkEnd(char symbol) {
+        if (checkWin(symbol)) {
+            if (symbol == DOT_HUMAN) {
+                System.out.println("HUMAN WINS!");
+            } else {
+                System.out.println("TERMINATOR WINS!");
+            }
+            return true;
+        }
+        if (checkDraw()) {
+            System.out.println("DRAW!");
+            return true;
+        }
+        return false;
     }
 
     private static void terminatorTurn() {
         int x, y;
         do {
-            x = RAND.nextInt(SIZE);
-            y = RAND.nextInt(SIZE);
+            x = RAND.nextInt(size);
+            y = RAND.nextInt(size);
         } while (!isValidCell(x, y));
-        System.out.println("Terminator's turn was: " + (x + 1) + " " + (y + 1));
-        map[y][x] = DOT_O;
+        System.out.println("TERMINATOR'S TURN was: " + (x + 1) + " " + (y + 1));
+        map[x][y] = DOT_TERMINATOR;
+        lastRow = x;
+        lastColomn = y;
+        turns_counter++;
     }
 
     private static boolean isValidCell(int x, int y) {
-        return x >= 0 && y >= 0 && x < SIZE && y < SIZE && map[y][x] == DOT_EMPTY;
+        return x >= 0 && y >= 0 && x < size && y < size && map[x][y] == DOT_EMPTY;
     }
 
-    private static boolean isMapFull() {
-        for (char[] arrX : map) {
-            for (char symbol : arrX) {
-                if (symbol == DOT_EMPTY) return false;
-            }
-        }
-        return true;
+    private static boolean checkDraw() {
+        return turns_counter >= size * size;
     }
 
     private static boolean checkWin(char symbol) {
-        int winX = 0;
-        int winY = 0;
-        int diag1 = 0;
-        int diag2 = 0;
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == symbol) {
-                    winX++;
-                }
-                if (map[j][i] == symbol) {
-                    winY++;
-                }
-                if (i == j && map[i][j] == symbol) {
-                    diag1++;
-                }
-                if (i + j == map.length - 1 && map[i][j] == symbol) {
-                    diag2++;
-                }
+        return checkWinH(symbol) || checkWinV(symbol) || checkWinD1(symbol); // || checkWinD2();
+    }
+
+    private static boolean checkWinH(char symbol) {
+        int counter = 0;
+        for (char s : map[lastRow]) {
+            if (s == symbol) {
+                counter++;
+            } else {
+                counter = 0;
             }
-            if (winX == DOTS_TO_WIN || winY == DOTS_TO_WIN || diag1 == DOTS_TO_WIN || diag2 == DOTS_TO_WIN) return true;
-            winX = 0;
-            winY = 0;
+            if (counter == dots_to_win) return true;
         }
         return false;
     }
+
+    private static boolean checkWinV(char symbol) {
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (map[i][lastColomn] == symbol) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if (counter == dots_to_win) return true;
+        }
+        return false;
+    }
+
+    private static boolean checkWinD1(char symbol) {
+        int counter = 0;
+        int startRowDiag1, endRowDiag1;
+
+        if (lastRow == lastColomn) {                        // проверяем ровно по диагонали
+            startRowDiag1 = 0;
+            for (int i = startRowDiag1; i < size; i++) {
+                if (map[i][i] == symbol) {
+                    counter++;
+                } else {
+                    counter = 0;
+                }
+                if (counter == dots_to_win) return true;
+            }
+        } else if (lastRow < lastColomn) {                   // проверяем в верх правой части диагонали
+            startRowDiag1 = 0;
+            for (int i = startRowDiag1; i < size - 1; i++) {
+                if (map[i][i + 1] == symbol) {
+                    counter++;
+                } else {
+                    counter = 0;
+                }
+                if (counter == dots_to_win) return true;
+            }
+        } else {                                             // проверяем в ниж левой части диагонали
+            startRowDiag1 = lastRow - lastColomn;
+            int j = 0;
+            for (int i = startRowDiag1; i < size; i++) {
+                if (map[i][j++] == symbol) {
+                    counter++;
+                } else {
+                    counter = 0;
+                }
+                if (counter == dots_to_win) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isContinueGame() {
+        System.out.println("Do you want to play one more time? (y/n): ");
+        return switch (SCAN.next()) {
+            case "y", "yes", "да", "1" -> true;
+            default -> false;
+        };
+    }
+
+    private static void endGame() {
+        System.out.println("GAME OVER!");
+        System.out.println("I hope to see you soon...");
+        SCAN.close();
+    }
+
 }
